9f5844a3576bcf4f7bc4f9f17d6a10f453281e9a
joeytxy <66881214+joeytxy@users.noreply.github.com>
2022-08-29 01:30:18 +0800

    Update anonymize_manual_input.py

--- anonymize_manual_input.py
+++ anonymize_manual_input.py
@@ -206,7 +206,7 @@ def anonymize_manual_input(user_input,package=['stanza'],union_intersection=None
             if 1 in additional_details:
                 final_return = re.sub(r"([sftg]\d{7}[a-z])", "[NRIC]", final_return,flags=re.IGNORECASE) 
             if 2 in additional_details:
-                final_return = re.sub(r"(\d{8})", "[PHONE]", final_return, flags=re.IGNORECASE)
+                final_return = re.sub(r"(\d{8})", "[PHONE]", final_return)
             if 3 in additional_details:
                 final_return = re.sub(r"(\d{10}[A-z])", "[CASENO]", final_return, flags=re.IGNORECASE)
             if 4 in additional_details:
