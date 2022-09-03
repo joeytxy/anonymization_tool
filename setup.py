0d1594f62c144ab60dce857f13737a97503fe909
joeytxy <66881214+joeytxy@users.noreply.github.com>
2022-09-03 16:05:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup,find_packages
 
 setup(
     name='anonymization_tool',
-    version='0.3',
+    version='0.4',
+    description='An anonymisation tool which utilises NER packages such as flair, NLTK,spaCy and stanza to mask personal names (default). Other information such as NRIC, phone number etc can also be masked by giving corresponding input.',
     license='MIT',
     author='Joey Tan Xin Yi',
     author_email='joeytxy0706@gmail.com',
@@ -13,9 +14,9 @@ setup(
     install_requires=[
         'nltk==3.6.1',
         'spacy==3.4.1',
+        'en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.0/en_core_web_sm-3.4.0.tar.gz',
         'flair==0.11.3',
         'stanza==1.4.0',
         'pandas==1.3.1'
-        ],
-    dependency_links=['https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.0/en_core_web_sm-3.4.0.tar.gz#egg=en_core_web_sm'],
+        ]
     )
