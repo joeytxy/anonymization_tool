b47d19a736ed5f5a1be185138a776b74cf74d0c8
joeytxy <66881214+joeytxy@users.noreply.github.com>
2022-09-03 13:29:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='anonymization_tool',
-    version='0.1',
+    version='0.2',
     license='MIT',
     author='Joey Tan Xin Yi',
     author_email='joeytxy0706@gmail.com',
@@ -12,7 +12,7 @@ setup(
     keywords='anonymization tool',
     install_requires=[
         'nltk==3.6.1',
-        'spacy==3.4.1',
+        'spacy==3.4.0',
         'en_core_web_sm==3.4.0',
         'flair==0.11.3',
         'stanza==1.4.0',
