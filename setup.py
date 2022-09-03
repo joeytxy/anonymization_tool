794d9832c281b6d6d6530dcba7503067e5d5a93f
joeytxy <66881214+joeytxy@users.noreply.github.com>
2022-09-03 15:32:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='anonymization_tool',
-    version='0.2',
+    version='0.3',
     license='MIT',
     author='Joey Tan Xin Yi',
     author_email='joeytxy0706@gmail.com',
@@ -12,8 +12,8 @@ setup(
     keywords='anonymization tool',
     install_requires=[
         'nltk==3.6.1',
-        'spacy==3.4.0',
-        'en_core_web_sm==3.4.0',
+        'spacy==3.4.1',
+        'https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.0/en_core_web_sm-3.4.0.tar.gz#egg=en_core_web_sm',
         'flair==0.11.3',
         'stanza==1.4.0',
         'pandas==1.3.1'
